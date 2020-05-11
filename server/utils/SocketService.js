47f345b8dda53d02da2c63b79636406f1d56b66b
hongfanmeng <hongfanmeng@gmail.com>
2020-05-11 22:13:37 +0800

    add navbar

--- server/utils/SocketService.js
+++ server/utils/SocketService.js
@@ -1,24 +1,39 @@
 const { GoGame, Player } = require("./GoGame");
 let newGame;
 let players = {};
+
+function judge(nickname) {
+  if (!nickname || !nickname.length) return 1;
+  //invalid name
+  else if (players[nickname]) return 2; //same name
+  return 0;
+}
 module.exports = function SocketService(socket, io) {
+  //new player
   let id = socket.id;
   let player = new Player(id, socket);
   if (!newGame) newGame = new GoGame();
   if (newGame.gameOver != 0 || newGame.isFull()) newGame = new GoGame();
   let currentGame = newGame;
 
+  //nickname query socket
   socket.on("nickname", (nickname) => {
-    if (!nickname || !nickname.length) {
-      socket.emit("color", -1); //invalid nickname
-      return;
-    }
-    if (players[nickname]) {
-      socket.emit("color", -2); //same nickname player online
-      return;
-    }
+    let errCode = judge(nickname);
+    socket.emit("err", errCode);
+    if (errCode) return;
+  });
+  socket.on("ready", (nickname) => {
+    //handling error
+    let errCode = judge(nickname);
+    socket.emit("err", errCode);
+    if (errCode) return;
+
+    //handling nickname
     players[nickname] = 1;
+    if (player.nickname) delete players[player.nickname];
     player.nickname = nickname;
+
+    //starting the game
     currentGame.addPlayer(player);
     socket.emit("color", player.color);
     if (currentGame.isFull()) {
@@ -27,6 +42,7 @@ module.exports = function SocketService(socket, io) {
       currentGame.emitAll("round", currentGame.turn);
     }
   });
+
   socket.on("position", (data) => {
     data = {
       x: data.x,
@@ -47,16 +63,14 @@ module.exports = function SocketService(socket, io) {
     if (currentGame.judge(data.x, data.y)) {
       currentGame.emitAll("gameOver", currentGame.gameOver);
     }
-    //console.log(currentGame.gameOver);
     currentGame.nextRound();
     currentGame.emitAll("round", currentGame.turn);
   });
 
   socket.on("disconnect", () => {
-    currentGame.gameOver = -1; //disconnectx
-    if (player.nickname) players[player.nickname] = 0;
+    currentGame.gameOver = -1; //disconnect
+    if (player.nickname) delete players[player.nickname];
     let another = currentGame.getAnother(player);
-    //console.log(another);
     if (another) another.socket.emit("gameOver", -1);
   });
 };
